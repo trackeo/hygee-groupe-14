4b1ad2f0bbeba22e1475a9fd000f8c7ee64c5a85
trackeo <wadoux.alexandre@gmail.com>
2018-07-04 23:42:34 +0200

    ajout slider

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,34 @@
  * @version 
  * Copyright 2018.  licensed.
  */
+$(document).ready(function(){
+$('.wrapper-slides').slick({
+  prevArrow: '<button class="slick-prev" type="button"></button>',
+  nextArrow: '<button class="slick-next" type="button"></button>',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplaySpeed: 2000,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1110,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        infinite: true,
+      }
+    },
+
+  ]
+});
+});
+
+
 (function ($, window, document, undefined) {
 
   'use strict';
@@ -21,5 +49,6 @@
       });
 
   });
+
 
 })(jQuery, window, document);
