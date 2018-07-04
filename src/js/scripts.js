dad0ee2ad8ae2524ecb07e28af55ea73469277a9
trackeo <wadoux.alexandre@gmail.com>
2018-07-04 23:42:34 +0200

    ajout slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,31 @@
+$(document).ready(function(){
+$('.wrapper-slides').slick({
+  prevArrow: '<button class="slick-prev" type="button"></button>',
+  nextArrow: '<button class="slick-next" type="button"></button>',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplaySpeed: 2000,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1110,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        infinite: true,
+      }
+    },
+
+  ]
+});
+});
+
+
 (function ($, window, document, undefined) {
 
   'use strict';
@@ -13,5 +41,6 @@
       });
 
   });
+
 
 })(jQuery, window, document);
